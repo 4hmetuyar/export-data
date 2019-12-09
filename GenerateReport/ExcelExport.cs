696c13b9720dad4749b79c231807bac015159485
Ahmet Uyar <4hmetuyar@gmail.com>
2019-12-09 16:13:29 +0300

    Generate excel.

---
+++ GenerateReport/ExcelExport.cs
@@ -0,0 +1,67 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Web;
+
+namespace GenerateReport
+{
+    public class ExcelExport<T>
+    {
+        /// <summary>
+        /// Export excel generic type
+        /// </summary>
+        /// <param name="enumerable"></param>
+        /// <param name="fileName"></param>
+        /// <param name="sheetName"></param>
+        /// <returns></returns>
+        public static bool ToExcelFile(IEnumerable<T> enumerable, string fileName, string sheetName)
+        {
+            try
+            {
+                Guid newGuid = Guid.NewGuid();
+
+                if (fileName == null) fileName = newGuid.ToString();
+                if (sheetName == null) sheetName = "sheet1";
+                DataTable convertToDataTable = ConvertToDataTable(enumerable);
+                using (XLWorkbook xlWorkbook = new XLWorkbook())
+                {
+                    xlWorkbook.Worksheets.Add(convertToDataTable, sheetName);
+                    var filePath = HttpContext.Current.Server.MapPath($"/content/upload/{fileName}.xlsx");
+                    xlWorkbook.SaveAs(filePath);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                //Logging.
+                return false;
+            }
+
+
+        }
+
+        /// <summary>
+        /// Convert Data Table for Generic Type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static DataTable ConvertToDataTable<T>(IEnumerable<T> data)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+    }
+}
--- GenericReport/Controllers/HomeController.cs
+++ GenericReport/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GenerateReport;
 
 namespace GenericReport.Controllers
 {
@@ -10,7 +11,40 @@ namespace GenericReport.Controllers
     {
         public ActionResult Index()
         {
+            List<Student> students = new List<Student>();
+            for (int i = 0; i < 1048575; i++)
+            {
+                students.Add(new Student
+                {
+                    Email = "asdads",
+                    Name = "asdasd",
+                    Surname = "safasdfasf",
+                    Email1 = "safddasfj adjflşdajsfşljsafdljdas",
+                    Email2 = "asdfas dfdajsfladsjf ladfsj",
+                    Email3 = "adsfj asdjf sahdofhsaodhfsadşfsad",
+                    Email4 = "a lsakjdfşlsadfj sajdfsa",
+                    Email5 = "a sadfjlsajdşfjsaldjfğoasf",
+                    Email6 = " asjdfljsadfpjpamfoısadfmsafoa"
+                });
+            }
+
+
+
+            ExcelExport<Student>.ToExcelFile(students, DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss"), null);
             return View();
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public string Email1 { get; set; }
+            public string Email2 { get; set; }
+            public string Email3 { get; set; }
+            public string Email4 { get; set; }
+            public string Email5 { get; set; }
+            public string Email6 { get; set; }
         }
 
         public ActionResult About()
